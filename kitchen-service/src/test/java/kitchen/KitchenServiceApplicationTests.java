4624be994330831aea913703b39c8bebc499cca7
andrejavukovic20 <vukovic.it64.2019@uns.ac.rs>
2025-08-31 14:57:36 +0200

    changing test in kitchen service

--- kitchen-service/src/test/java/kitchen/KitchenServiceApplicationTests.java
+++ kitchen-service/src/test/java/kitchen/KitchenServiceApplicationTests.java
@@ -4,7 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+		  properties = {
+		    "spring.autoconfigure.exclude=" +
+		    "org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration," +
+		    "org.springframework.boot.autoconfigure.amqp.RabbitAnnotationDrivenConfiguration",
+		    "spring.rabbitmq.listener.simple.auto-startup=false",
+		    "spring.rabbitmq.listener.direct.auto-startup=false"
+		})
 @ActiveProfiles("test")
 public class KitchenServiceApplicationTests {
 
