2628667ed51cabdcae2e954e12b9d6e18f239e93
andrejavukovic20 <vukovic.it64.2019@uns.ac.rs>
2025-08-31 14:47:46 +0200

    fixing bug in kitchen service

--- kitchen-service/src/test/java/kitchen/KitchenServiceApplicationTests.java
+++ kitchen-service/src/test/java/kitchen/KitchenServiceApplicationTests.java
@@ -2,8 +2,10 @@ package kitchen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class KitchenServiceApplicationTests {
 
 	   @Test
